3695375777cfe4edfb5d56eb73fd58ecb8a6320c
Nitin Sheoran <nsheoran2001@gmail>
2023-11-21 11:15:03 +0530

    liked button activity

--- lib/model/Product_model.dart
+++ lib/model/Product_model.dart
@@ -12,7 +12,7 @@ class ProductResponse {
     if (json['products'] != null) {
       products = <Products>[];
       json['products'].forEach((v) {
-        products!.add(new Products.fromJson(v));
+        products!.add(Products.fromJson(v));
       });
     }
     total = json['total'];
@@ -21,13 +21,13 @@ class ProductResponse {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.products != null) {
-      data['products'] = this.products!.map((v) => v.toJson()).toList();
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (products != null) {
+      data['products'] = products!.map((v) => v.toJson()).toList();
     }
-    data['total'] = this.total;
-    data['skip'] = this.skip;
-    data['limit'] = this.limit;
+    data['total'] = total;
+    data['skip'] = skip;
+    data['limit'] = limit;
     return data;
   }
 }
---
+++ lib/service/shared_preference_service.dart
@@ -0,0 +1,53 @@
+
+import 'package:product_test_app/model/products_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefService {
+  static String productKey = 'layout_key';
+  static const likedCount = 'likedCount_';
+
+  static Future setLikedCount(int productId, int likedCount) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('$likedCount$productId', likedCount);
+  }
+
+  static Future<int> getLikedCount(int productId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('$likedCount$productId') ?? 0;
+  }
+
+  static Future setProduct(bool isList) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    sf.setBool(productKey, isList);
+  }
+
+  static Future<bool> getProduct() async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    bool value = sf.getBool(productKey) ?? true;
+    return value;
+  }
+
+  static Future<void> updateLikedProducts(
+      Products product, bool isLiked) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> likedProducts = prefs.getStringList('likedProducts') ?? [];
+    String productDetails = "${product.title},"
+        "${product.price},"
+        "${product.discountPercentage},"
+        "${product.description},"
+        "${product.images!.first}";
+
+    if (isLiked && !likedProducts.contains(productDetails)) {
+      likedProducts.add(productDetails);
+    } else if (!isLiked) {
+      likedProducts.remove(productDetails);
+    }
+
+    prefs.setStringList('likedProducts', likedProducts);
+  }
+
+// static Future<List<String>> getLikedProducts() async {
+//   SharedPreferences prefs = await SharedPreferences.getInstance();
+//   return prefs.getStringList('likedProducts') ?? [];
+// }
+}
